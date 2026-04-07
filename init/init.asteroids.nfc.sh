e3ff766d06d8fe2448b310e870f9eedb89d6dddd
rbnj <rbnj2112@gmail.com>
2026-04-07 21:53:06 +0300

    asteroids: sepolicy: Revert lineage specific changes for nfc prop
    Revert "asteroids: Re-configure NFC properties"
    
    This reverts commit 52c33383cd49ff24c1f3541ea3d091c34193c5ca.
    
    Revert "asteroids: Remove duplicate vendor_nfc_prop type"
    
    This reverts commit 337cf994d1fe7daf832587ff9cc0bf0a13aa4393.

--- init/init.asteroids.nfc.sh
+++ init/init.asteroids.nfc.sh
@@ -10,23 +10,23 @@ if [ -f /sys/bus/i2c/devices/1-0008/hw_version ]; then
         "ST21")
             case "$pbid" in
                 "Base")
-                    setprop vendor.nfc.config_file_name "libnfc-hal-st21-BASE.conf"
+                    setprop persist.vendor.nfc.config_file_name "libnfc-hal-st21-BASE.conf"
                     ;;
                 "Pro")
-                    setprop vendor.nfc.config_file_name "libnfc-hal-st21-PRO.conf"
+                    setprop persist.vendor.nfc.config_file_name "libnfc-hal-st21-PRO.conf"
                     ;;
             esac
 
-            setprop vendor.nfc_model "ST21"
+            setprop persist.vendor.nfc_model "ST21"
             ;;
         "ST54")
             if [ "$sku" = "JPN" ]; then
-                setprop vendor.nfc.config_file_name "libnfc-hal-st54j-JPN.conf"
+                setprop persist.vendor.nfc.config_file_name "libnfc-hal-st54j-JPN.conf"
             else
-                setprop vendor.nfc.config_file_name "libnfc-hal-st54j-PRO.conf"
+                setprop persist.vendor.nfc.config_file_name "libnfc-hal-st54j-PRO.conf"
             fi
 
-            setprop vendor.nfc_model "ST54"
+            setprop persist.vendor.nfc_model "ST54"
             ;;
     esac
 fi
