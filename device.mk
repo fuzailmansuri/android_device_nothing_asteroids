3240e94d26e30f23e8fc3a09b1cc64356b5bed91
rbnj <rbnj2112@gmail.com>
2026-05-22 11:13:27 +0300

    asteroids: Drop ActionButton package

--- device.mk
+++ device.mk
@@ -227,6 +227,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
